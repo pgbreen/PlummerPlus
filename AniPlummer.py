4827c3d92d8b2b23243355454d91340a9e6f759d
Phil Breen <pbreen@ITs-MacBook-Pro.local>
2018-06-15 16:58:30 +0100

    updated

--- AniPlummer.py
+++ AniPlummer.py
@@ -4,7 +4,7 @@
 #
 # Description: Generates realisation of anisotropic and rotating models with plummer model density distribution
 # 		Note on units G=M=a=1 for generation but values are rescales to Henon units for output 
-#		(see scale factors lfact,vfact, default output file fort.10)
+#		(see scale factors: lfact=3.0*pi/16.0, vfact=1/lfact. default output file fort.10)
 #
 # AUTHOR: Phil Breen, University of Edinburgh
 # 
@@ -15,6 +15,7 @@
 # CITATION: Please use Breen, Varri, Heggie 2018 (https://arxiv.org/abs/)
 #
 # EXAMPLES:
+#       use "chmod a+x AniPlummer.py" to make script executable, alternativaly use "python AniPlummer.py ...."
 #
 # 	isotropic plummmer with 8K particles  
 #	./AniPlummer.py -n 8192
@@ -39,7 +40,7 @@
 # 	for a high shear model, set offset to 180 degree (i.e. -z) and used mass fraction 0.67 (i.e. rotatin model with 0 net L!) 
 # 	./AniPlummer.py -n 10000  -a 1.0 -oa 180.0 1.0 0.67
 #
-#	create mass segrated model most bound 50% of the mass consist of particles 10.0 times more massive then the least
+#	create mass segrated model most bound 50% of the mass consisting of particles 10.0 times more massive then the least
 #	./AniPlummer.py -n 10000 -ms 0.5 10.0
 #
 """ AniPlummer.py: generates realisation of anisotropic and rotating models with plummer model density distribution """
@@ -359,7 +360,7 @@ else:
 
 #---------------------------------add rotation------------------------------------
 
-
+countflip = 0
 if args.oa[2] >= 0.0:
 	# general trick energy cut and 
 	E = 0.5*np.power(v,2.0) - np.reciprocal(np.sqrt(np.power(x,2.0) + 1.0))
@@ -369,7 +370,7 @@ if args.oa[2] >= 0.0:
 	theta = pi*args.oa[0]/180.0
 	ov = np.array([0.0,sin(theta ),cos(theta )])
 	L = np.cross(w[:,1:4],w[:,4:])	
-	countflip = 0
+	
 	for i in xrange(args.n):	
 		if L[i,2] < 0.0 and E[i] < ecut:
 			if args.a > np.random.rand():
@@ -436,7 +437,7 @@ np.savetxt(args.o, w)
 if args.e:
 	name = " Einstein sphere"
 elif args.ra > 0:
-	name = "  Osipkov-Merritt radially anisotropic (ra={})".format(args.ra)
+	name = " Osipkov-Merritt radially anisotropic (ra={})".format(args.ra)
 elif args.q != 0:
 	name = " Dejonghe (1987) anisotropic q={}".format(args.q)
 else:
@@ -471,14 +472,56 @@ else:
 			break
 
 print " rh = {} K.E. = {} vt^2 = {} vr^2 = {} ".format(rh,0.5*(svt2+svr2) ,svt2,svr2)
-if not args.e:
+if True:
+#if not args.e:
 	print " 1-0.5*<vt^2>/<vr^2> = {} ".format(1.0 - 0.5*svt2/svr2)
-	print " 2.0Tr/Tw = {} (Polyachenko and Shukhman 1973, crit value 1.7 +/- 0.25) ".format(2.0*svr2/svt2)
+	print " 2.0Tr/Tp = {} (Polyachenko and Shukhman 1973, crit value 1.7 +/- 0.25) ".format(2.0*svr2/svt2)
 
-if args.a > 0 or args.oa[0] > 0:
+#if args.a > 0 or args.oa[0] > 0:
 	L = np.multiply(w[:,0,None],np.cross(w[:,1:4],w[:,4:]))
 	L = np.sum(L, axis=0)
 	print " L = [{},{},{}]  |L|={} nf={}".format(L[0],L[1],L[2],np.linalg.norm(L),countflip )
+
+#
+# bins size, number of rings nbin^2, for nbin=10 each ring contains 1% mass
+#
+def sign(x):
+	if x >= 0:
+		return +1.0
+	else:
+		return -1.0
+#hack use pre cal R2,|z| bins as rho fixed
+rindex = [0.0385891298799, 0.0866921555876, 0.148588985051, 0.230883647572, 0.345272808206,
+0.517942199325,0.807197126028, 1.38667629541, 3.10347289646]
+
+nbin = 10
+R2 = np.sum(np.power(w[:,1:3],2), axis=1)
+z = np.abs(w[:,3])
+indx = sorted(range(args.n),key=lambda k: R2[k])
+Rbins = np.array_split(indx,nbin)
+vi = w[:,4:]
+xi = np.zeros((args.n,3))
+xi[:,0] = w[:,2]
+xi[:,1] = -w[:,1]
+xi = np.divide(xi,np.sqrt(R2)[:,None])
+vphi = xi[:,0]*vi[:,0] + xi[:,1]*vi[:,1]
+vphike = 0.0
+for rl in Rbins:
+	zi =  z[rl]
+	indxi = sorted(range(len(zi)),key=lambda k: zi[k])
+	zbins = np.array_split(indxi,nbin)
+	#print R2[rl[0]],R2[rl[-1]]
+	for zl in zbins:
+		
+		print "{:.2e}".format(zi[zl[-1]]),
+		vphiavg = np.sum(vphi[rl[zl]])
+		vsign = sign(vphiavg)
+		#mass = np.sum(w[rl[zl],0])
+		vphike += vsign*(vphiavg**2)/len(zl)
+	print ""
+
+
+print " T_phi/|pot| = {}, (assuming pot = 0.5)".format(vphike/args.n)
 print "\n"
 
 
